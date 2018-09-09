d3b53d23e8c27d11d2662f7b7cbeb6c3a774478f
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-09-09 14:32:54 -0500

    funcionalidad de notificacion

---
+++ src/js/notification.js
@@ -0,0 +1,24 @@
+ // prueba 
+ console.log("todo va bien");
+ 
+
+
+let myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  let service_id = "default_service";
+  let template_id = "template_FcvJzdSf";
+
+  myform.find("button").text("Enviando...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("La notificacion se envio correctamente!");
+       myform.find("button").text("Enviado");
+    }, function(err) {
+       alert("Hubo un error vuelve a intentar!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Intenta mas tarde.");
+    });
+  return false;
+});
+
