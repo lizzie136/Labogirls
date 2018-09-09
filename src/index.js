104dc332de8da3e8d58f9f2c26bfbd8e9f417f46
lizzie136 <darknessover136@gmail.com>
2018-09-09 16:24:29 -0500

    Update index.js to use firebase when needed

--- src/index.js
+++ src/index.js
@@ -1,75 +1,80 @@
-
 import moment from 'moment';
 import superLogin from './js/login';
-import { generatePaymentString, generateQR }from './js/qrgenerator';
+import {
+  generatePaymentString,
+  generateQR
+} from './js/qrgenerator';
+import { isNull } from 'util';
 
 
 superLogin();
 
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
   /// FIREBASE 
   var config = {
-      apiKey: "AIzaSyCoZYLKMfBv8NWc5J4eprfuzWeoRHrCYcA",
-      authDomain: "payqrdash.firebaseapp.com",
-      databaseURL: "https://payqrdash.firebaseio.com",
-      projectId: "payqrdash",
-      storageBucket: "payqrdash.appspot.com",
-      messagingSenderId: "103281908726"
+    apiKey: "AIzaSyCoZYLKMfBv8NWc5J4eprfuzWeoRHrCYcA",
+    authDomain: "payqrdash.firebaseapp.com",
+    databaseURL: "https://payqrdash.firebaseio.com",
+    projectId: "payqrdash",
+    storageBucket: "payqrdash.appspot.com",
+    messagingSenderId: "103281908726"
   };
-  firebase.initializeApp(config);
-  const db = firebase.firestore();
 
-  const imprime = document.getElementById("total");
-
-  const boton = document.getElementById("crear");
-  boton.addEventListener("click", e => {
-    const selectElement = document.getElementById("products");
-    const selectedProduct = selectElement.options[selectElement.selectedIndex];
-    let producto = selectedProduct.value;
-    console.log(producto);
-    let precio = selectedProduct.dataset.price;
-    console.log(precio);
-    let cantidad = document.getElementById("quantity").value;
-    let suma = precio*cantidad;
-    console.log(suma);
-    imprime.innerHTML = `${suma}`;
-
+  if(typeof firebase !== 'undefined'){
+    firebase.initializeApp(config);
     const db = firebase.firestore();
 
-    db.collection("ventas").add({
-        product: producto,
-        preci: precio,
-        canti: cantidad
-    })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-        document.getElementById("products").selectedIndex = 0;
-        document.getElementById("quantity").value = "";
-        document.getElementById("total").value = "";
+    const imprime = document.getElementById("total");
 
-        const str = generatePaymentString({
+    const boton = document.getElementById("crear");
+    boton.addEventListener("click", e => {
+      const selectElement = document.getElementById("products");
+      const selectedProduct = selectElement.options[selectElement.selectedIndex];
+      let producto = selectedProduct.value;
+      console.log(producto);
+      let precio = selectedProduct.dataset.price;
+      console.log(precio);
+      let cantidad = document.getElementById("quantity").value;
+      let suma = precio * cantidad;
+      console.log(suma);
+      imprime.innerHTML = `${suma}`;
+
+      const db = firebase.firestore();
+
+      db.collection("ventas").add({
+          product: producto,
+          preci: precio,
+          canti: cantidad
+        })
+        .then(function (docRef) {
+          console.log("Document written with ID: ", docRef.id);
+          document.getElementById("products").selectedIndex = 0;
+          document.getElementById("quantity").value = "";
+          document.getElementById("total").value = "";
+
+          const str = generatePaymentString({
             motive: producto,
             amount: cantidad,
             date: moment().format('DD/MM/YYYY'),
             userAccount: `${BBVABANCOMER_USER_ACCOUNT}`,
             description: 'Abarrotes Meche'
+          });
+          window.localStorage.setItem('qrdata', str);
+          window.location = '/qrgenerator.html';
+        })
+        .catch(function (error) {
+          console.error("Error adding document: ", error);
         });
-        window.localStorage.setItem('qrdata', str);
-        window.location = '/qrgenerator.html';
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
     });
-  });
+}
+//Leer documentos
+let ventas = document.getElementById('tableBody');
 
-  //Leer documentos
-  let ventas = document.getElementById('tableBody');
-
-  ventas && db.collection('ventas').onSnapshot((querySnapshot) => {
-    ventas.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().id}`);
-        ventas.innerHTML += `
+ventas && db.collection('ventas').onSnapshot((querySnapshot) => {
+  ventas.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data().id}`);
+    ventas.innerHTML += `
             <tr>
               <td>${doc.data().id}</td>
               <td>${doc.data().canti}</td>
@@ -78,13 +83,13 @@ window.addEventListener('load', ()=> {
               <td>${doc.data().cliente}</td>
               <td>${doc.data().date} ${doc.data().time}</td>
             </tr>`;
-    });
   });
+});
 });
 
 const baseCanvas = document.getElementById('generated-qr');
-if(baseCanvas) {
-    const str = window.localStorage.getItem('qrdata');
-    generateQR(str, baseCanvas, (err) => console.log(err));
+if (baseCanvas) {
+  const str = window.localStorage.getItem('qrdata');
+  generateQR(str, baseCanvas, (err) => console.log(err));
 
 }
